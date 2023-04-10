c96df52d853a4015ece087b09cda1b4b7293b6db
Eunsuhzzang <anna123234@naver.com>
2023-04-10 18:07:36 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+            <h1>About Eunsuh's hobby :0</h1> 
+            <h2>Watching musical!</h2>
+            <h3>Watching musical makes me experience various genre ^&^ </h3>
+            <Link href="/">Back to home</Link> 
+        </div>
+    )
+}
